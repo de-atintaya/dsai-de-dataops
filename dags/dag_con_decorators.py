06bcc773bdd2d58e452aab7ca59c8fd35bf4162a
AngelTintaya <a.tintaya.m@gmail.com>
2025-06-12 23:06:55 -0500

    dataops: Adding dag with decorators

---
+++ dags/dag_con_decorators.py
@@ -0,0 +1,23 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+def generar_saludo(nombre):
+    return f"Hola, {nombre}!"
+
+@dag(
+    dag_id="dag_con_decorators",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["ejemplo"]
+)
+def flujo_de_saludo():
+    
+    @task
+    def saludar():
+        mensaje = generar_saludo("Airflow")
+        print(mensaje)
+
+    saludar()  # ejecución de la tarea
+
+dag = flujo_de_saludo()
